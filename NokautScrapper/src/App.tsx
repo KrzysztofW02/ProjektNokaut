ca585aa960945c916c705df8690c91564ee5096f
Krzysztof Wasik <Krzysztof.Wasik2002@gmail.com>
2024-03-08 19:20:40 +0100

    Added products display on grid + search backend

--- NokautScrapper/src/App.tsx
+++ NokautScrapper/src/App.tsx
@@ -1,24 +1,27 @@
-import { useState } from "react";
 import "./App.css";
 import { GetProductsList } from "./WebScrapper/WebScrapper";
 import Navbar from "./components/Navbar";
 import Grid from "./components/Grid";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-
   let ProductData: Array<Product> = [];
 
-  GetProductsList("hulajnoga elektryczna").then((data) => {
+  /*GetProductsList("hulajnoga elektryczna").then((data) => {
     ProductData = data;
     console.log(ProductData);
   });
+*/
+  const handleSearch = (searchText: string) => {
+    GetProductsList(searchText).then((data) => {
+      ProductData = data;
+      console.log(ProductData);
+    });
+  };
 
   return (
     <>
       <div>
-        <Navbar />
+        <Navbar onSearch={handleSearch} />
       </div>
 
       <div className="mt-5">
