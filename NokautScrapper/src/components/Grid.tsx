bdb2b15793b10a6a36c53ee8361390e3e6bd1571
Krzysztof Wasik <Krzysztof.Wasik2002@gmail.com>
2024-03-18 19:40:39 +0100

    Added automatic page loading

--- NokautScrapper/src/components/Grid.tsx
+++ NokautScrapper/src/components/Grid.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Row, Col, Card, Button } from "react-bootstrap";
 import "react-bootstrap";
 import { GetProductsList } from "../WebScrapper/WebScrapper";
@@ -15,65 +15,77 @@ interface Product {
 function Grid({ productToSearch }: { productToSearch: string }) {
   const [products, setProducts] = useState<Product[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const bottomBoundaryRef = useRef<HTMLDivElement>(null);
 
   const fetchNextPage = () => {
+    setLoading(true);
     const nextPage = currentPage + 1;
     GetProductsList(productToSearch, nextPage).then((data) => {
       setProducts([...products, ...data]);
       setCurrentPage(nextPage);
+      setLoading(false);
     });
   };
 
   useEffect(() => {
-    GetProductsList(productToSearch).then((data) => {
-      setProducts(data);
-    });
-  }, [productToSearch]);
+    const handleScroll = () => {
+      if (
+        bottomBoundaryRef.current &&
+        window.innerHeight + window.scrollY >=
+          bottomBoundaryRef.current.offsetTop
+      ) {
+        fetchNextPage();
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [products]);
 
   return (
     <div className="MarginGridTop MainContainerSize">
       <div>
-        {products.length > 0 ? (
-          <>
-            <Row style={{ marginBottom: "15px" }}>
-              {products.map((product, index) => (
-                <Col key={index} xs={4} md={4} style={{ marginBottom: "30px" }}>
-                  <Card className="Card">
-                    <Card.Link
-                      style={{ textDecoration: "none", color: "black" }}
-                      href={product.sellerUrl}
-                    >
-                      <Card.Img variant="top" src={product.image} alt="" />
-                      <Card.Body>
-                        <Card.Title
-                          style={{
-                            fontSize: "15px",
-                            textDecoration: "none",
-                            textWrap: "nowrap",
-                          }}
-                        >
-                          {product.title}
-                        </Card.Title>
-                        <Card.Text
-                          style={{
-                            fontSize: "20px",
-                            textDecoration: "none",
-                            color: "red",
-                          }}
-                        >
-                          {product.price}
-                        </Card.Text>
-                      </Card.Body>
-                    </Card.Link>
-                  </Card>
-                </Col>
-              ))}
-            </Row>
-            <Button onClick={fetchNextPage}>Show More</Button>
-          </>
-        ) : (
-          <p className="Loading">Loading...</p>
+        {products.length > 0 && (
+          <Row style={{ marginBottom: "15px" }}>
+            {products.map((product, index) => (
+              <Col key={index} xs={4} md={4} style={{ marginBottom: "30px" }}>
+                <Card className="Card">
+                  <Card.Link
+                    style={{ textDecoration: "none", color: "black" }}
+                    href={product.sellerUrl}
+                  >
+                    <Card.Img variant="top" src={product.image} alt="" />
+                    <Card.Body>
+                      <Card.Title
+                        style={{
+                          fontSize: "15px",
+                          textDecoration: "none",
+                          textWrap: "nowrap",
+                        }}
+                      >
+                        {product.title}
+                      </Card.Title>
+                      <Card.Text
+                        style={{
+                          fontSize: "20px",
+                          textDecoration: "none",
+                          color: "red",
+                        }}
+                      >
+                        {product.price}
+                      </Card.Text>
+                    </Card.Body>
+                  </Card.Link>
+                </Card>
+              </Col>
+            ))}
+          </Row>
         )}
+        {loading && <p className="Loading">Loading...</p>}
+        <div ref={bottomBoundaryRef}></div>
       </div>
     </div>
   );
