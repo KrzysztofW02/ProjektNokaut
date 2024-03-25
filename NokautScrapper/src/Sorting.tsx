27d631438791cd4659ba9db24413a35061e9bc27
Patryk Ihas <97395198+ihasp@users.noreply.github.com>
2024-03-25 19:03:35 +0100

    Changed var to const, since we wont change this part of code anyway

--- NokautScrapper/src/Sorting.tsx
+++ NokautScrapper/src/Sorting.tsx
@@ -1,7 +1,6 @@
 
 export function sortDataByAscending(Products: Product[], changeProducts:(newProducts: any) => void): Product[] {
-
-  var SortedProducts = Products
+  const SortedProducts = Products
     .filter((item) => item && typeof item.price === "string") // Filter out null or items without price
     .slice()
     .sort((a, b) => parseFloat(a.price.replace(/\D/g, "")) - parseFloat(b.price.replace(/\D/g, "")));
@@ -10,8 +9,7 @@ export function sortDataByAscending(Products: Product[], changeProducts:(newProd
 }
 
 export function sortDataByDescending(Products: Product[], changeProducts:(newProducts: any) => void): Product[] {
-
-  var SortedProducts = Products
+  const SortedProducts = Products
     .filter((item) => item && typeof item.price === "string") // Filter out null or items without price
     .slice()
     .sort((a, b) => parseFloat(b.price.replace(/\D/g, "")) - parseFloat(a.price.replace(/\D/g, "")));
